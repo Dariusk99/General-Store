646aa4e7447440077b66fa379d732451f3d37e17
Dariusk99 <dariuszkosnicki@gmail.com>
2025-06-27 16:14:54 +0200

    prepare packages and pom dependencies

---
+++ src/main/java/org/generalstore/GeneralStore.java
@@ -0,0 +1,11 @@
+package org.generalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralStore {
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralStore.class, args);
+    }
+}
